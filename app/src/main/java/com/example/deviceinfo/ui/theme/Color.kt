fb3e374a31f6edcc471f6d9237cc761fc47f6c7d
chouaib Mounaime <chouaib.mounaime@diabeloop.fr>
2022-06-28 17:42:36 +0200

    init project

---
+++ app/src/main/java/com/example/deviceinfo/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.deviceinfo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val chargerGreen = Color(0xFF29BA55)
